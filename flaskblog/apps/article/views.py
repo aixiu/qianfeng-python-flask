0908160a70a2089b5aba1fb1f5e245027ee2ab4c
aixiu <4815563@qq.com>
2023-11-01 22:57:29 +0800

    点赞代码完毕

--- flaskblog/apps/article/views.py
+++ flaskblog/apps/article/views.py
@@ -1,9 +1,16 @@
-from flask import Blueprint, g, redirect, render_template, request, url_for
+from flask import Blueprint, g, jsonify, redirect, render_template, request, url_for
 from apps.user.models import User
 from apps.article.models import Article, Article_type
 from exts import db
 
 article_bp1 = Blueprint('article1', __name__, url_prefix='/article')
+
+
+# 自定义过滤器  python3 字符串默认是Unicode编码，不再需要进行解码操作
+@article_bp1.app_template_filter('cdecode')
+def content_decode(content):
+    # content = content.decode('utf-8')
+    return f'{content}'
 
 # 添加文章
 @article_bp1.route('/publish', methods=['POST', 'GET'])
@@ -24,3 +31,26 @@ def publish_article():
         db.session.commit()
         
         return redirect(url_for('user1.index'))
+    
+@article_bp1.route('/detail')
+def article_detail():
+    # 获取文章对象通过id
+    article_id = request.args.get('aid')
+    article = Article.query.get(article_id)
+    # 获取文章分类
+    types = Article_type.query.all()
+
+    return render_template('article/detail.html', article=article, types=types)
+
+@article_bp1.route('/love')
+def article_love():
+    article_id = request.args.get('aid')
+    tag = request.args.get('tag')
+
+    article = Article.query.get(article_id)
+    if tag == '1':
+        article.love_num -= 1
+    else:
+        article.love_num += 1
+    db.session.commit()
+    return jsonify(num=article.love_num)
